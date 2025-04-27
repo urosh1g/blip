31251654a09d198dd774653ffa7375ce9c28ad67
dzodzooo <stojanovicjovana312@gmail.com>
2025-04-27 16:25:42 +0200

    added callbacks

--- main.c
+++ main.c
@@ -7,6 +7,26 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+	glViewport(0,0,width,height);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+	//add necessary key checks here
+}
+
+void mouse_callback(GLFWwindow* window, int button, int action, int mods){
+	//add necessary mouse checks here
+}
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+	//add necessary onscroll actions here
+}
+
+void error_callback(int error_code, const char* description){
+	fprintf(stderr,"glfw error code: %d\n",error_code);
+	fprintf(stderr,"%s\n",description);
+	exit(EXIT_FAILURE);
+}
 
 GLFWwindow* init_glad_glfw(){
 	if(!glfwInit()){
@@ -24,15 +44,20 @@ GLFWwindow* init_glad_glfw(){
 		exit(EXIT_FAILURE);
 	}
 	glfwMakeContextCurrent(window);
-		
+	
+	glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetMouseButtonCallback(window,mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetErrorCallback(error_callback);
+	
 	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		glfwDestroyWindow(window);
+		glfwTerminate();
 		fprintf(stderr,"Failed to init glad\n");
 		exit(EXIT_FAILURE);
 	}
 	return window;
-}
-
-GLFWwindow* window_create_and_make_current(){
 }
 
 void main(){
