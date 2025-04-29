86766043a4205e17ef56e34a1a774beec55a4b66
Urosh <urosh_h@protonmail.com>
2025-04-29 13:06:44 +0200

    add more dynamic array functions

--- include/datastructs/dynarr.h
+++ include/datastructs/dynarr.h
@@ -9,40 +9,73 @@
 #define DYNARR_DEFAULT_CAPACITY 64
 #endif
 
-#define dynarr_define_for(type, name) \
-    typedef struct { \
-        type* elems; \
-        size_t capacity; \
-        size_t length; \
-    } dynarr_##name##_t; \
-    \
-    static inline void dynarr_##name##_init(dynarr_##name##_t* arr) { \
-        arr->elems = (type*)malloc(DYNARR_DEFAULT_CAPACITY * sizeof(type)); \
-        arr->length = 0; \
-        arr->capacity = DYNARR_DEFAULT_CAPACITY; \
-    } \
-    \
-    static inline void dynarr_##name##_init_capacity(dynarr_##name##_t* arr, size_t capacity) { \
-        arr->elems = (type*)malloc(capacity * sizeof(type)); \
-        arr->length = 0; \
-        arr->capacity = capacity; \
-    } \
-    \
-    static inline void dynarr_##name##_push(dynarr_##name##_t* arr, type value) { \
-        if (arr->length >= arr->capacity) { \
-            arr->capacity *= 2; \
-            arr->elems = realloc(arr->elems, arr->capacity * sizeof(type)); \
-        } \
-        arr->elems[arr->length++] = value; \
-    } \
-    \
-    static inline void dynarr_##name##_destroy(dynarr_##name##_t* arr) { \
-        free(arr->elems); \
-        arr->elems = NULL; \
-        arr->length = 0; \
-        arr->capacity = 0; \
-    } \
-    \
+#define dynarr_define_for(type, name)                                          \
+    typedef struct {                                                           \
+        type* elems;                                                           \
+        size_t capacity;                                                       \
+        size_t length;                                                         \
+    } dynarr_##name##_t;                                                       \
+                                                                               \
+    static inline void dynarr_##name##_init(dynarr_##name##_t* arr) {          \
+        arr->elems = (type*)malloc(DYNARR_DEFAULT_CAPACITY * sizeof(type));    \
+        arr->length = 0;                                                       \
+        arr->capacity = DYNARR_DEFAULT_CAPACITY;                               \
+    }                                                                          \
+                                                                               \
+    static inline void dynarr_##name##_init_capacity(dynarr_##name##_t* arr,   \
+                                                     size_t capacity) {        \
+        arr->elems = (type*)malloc(capacity * sizeof(type));                   \
+        arr->length = 0;                                                       \
+        arr->capacity = capacity;                                              \
+    }                                                                          \
+                                                                               \
+    static inline void dynarr_##name##_push(dynarr_##name##_t* arr,            \
+                                            type value) {                      \
+        if (arr->length >= arr->capacity) {                                    \
+            arr->capacity *= 2;                                                \
+            arr->elems = realloc(arr->elems, arr->capacity * sizeof(type));    \
+        }                                                                      \
+        arr->elems[arr->length++] = value;                                     \
+    }                                                                          \
+                                                                               \
+    static inline void dynarr_##name##_destroy(dynarr_##name##_t* arr) {       \
+        free(arr->elems);                                                      \
+        arr->elems = NULL;                                                     \
+        arr->length = 0;                                                       \
+        arr->capacity = 0;                                                     \
+    }                                                                          \
+                                                                               \
+    static inline type* dynarr_##name##_get(dynarr_##name##_t* arr,            \
+                                            size_t index) {                    \
+        if (index >= arr->length) {                                            \
+            return NULL;                                                       \
+        }                                                                      \
+        return &arr->elems[index];                                             \
+    }                                                                          \
+                                                                               \
+    static inline void dynarr_##name##_remove(dynarr_##name##_t* arr,          \
+                                              size_t index) {                  \
+        for (size_t i = index; i < arr->length - 1; i++) {                     \
+            arr->elems[i] = arr->elems[i + 1];                                 \
+        }                                                                      \
+        --arr->length;                                                         \
+    }                                                                          \
+                                                                               \
+    static inline void dynarr_##name##_remove_unordered(                       \
+        dynarr_##name##_t* arr, size_t index) {                                \
+        if (index >= arr->length) {                                            \
+            return;                                                            \
+        }                                                                      \
+        arr->elems[index] = arr->elems[--arr->length];                         \
+    }                                                                          \
+                                                                               \
+    static inline void dynarr_##name##_clear(dynarr_##name##_t* arr) {         \
+        arr->length = 0;                                                       \
+    }                                                                          \
+                                                                               \
+    static inline void dynarr_##name##_sort(                                   \
+        dynarr_##name##_t* arr, int (*cmp)(const void*, const void*)) {        \
+        qsort(arr->elems, arr->length, sizeof(*arr->elems), cmp);              \
+    }
 
 #endif
-
