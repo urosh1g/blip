7be695e8c7f6876aef3c02c496cdab7a37983bbd
dzodzooo <stojanovicjovana312@gmail.com>
2025-05-04 20:16:02 +0200

    format dequeue

--- include/datastructs/dequeue.h
+++ include/datastructs/dequeue.h
@@ -42,19 +42,21 @@
                                                                                \
     static inline void dequeue_##name##_add_back(dequeue_##name##_t* dequeue,  \
                                                  type el) {                    \
-        if(dequeue->length>=dequeue->capacity){\
-		size_t new_capacity=dequeue->capacity*2;\
-		size_t head_size=dequeue->head+dequeue->length;\
-		head_size-=head_size>dequeue->capacity?dequeue->capacity:0;\
-		dequeue->elems=realloc(dequeue->elems, new_capacity*sizeof(type));\
-		for(size_t i=0; i<head_size;i++){\
-			size_t old_index=dequeue->capacity-i;\
-			size_t new_index=new_capacity-i;\
-			dequeue->elems[new_index]=dequeue->elems[old_index];\
-		}\
-		dequeue->head=new_capacity-head_size;\
-		dequeue->capacity=new_capacity;\
-	}\
+        if (dequeue->length >= dequeue->capacity) {                            \
+            size_t new_capacity = dequeue->capacity * 2;                       \
+            size_t head_size = dequeue->head + dequeue->length;                \
+            head_size -=                                                       \
+                head_size > dequeue->capacity ? dequeue->capacity : 0;         \
+            dequeue->elems =                                                   \
+                realloc(dequeue->elems, new_capacity * sizeof(type));          \
+            for (size_t i = 0; i < head_size; i++) {                           \
+                size_t old_index = dequeue->capacity - i;                      \
+                size_t new_index = new_capacity - i;                           \
+                dequeue->elems[new_index] = dequeue->elems[old_index];         \
+            }                                                                  \
+            dequeue->head = new_capacity - head_size;                          \
+            dequeue->capacity = new_capacity;                                  \
+        }                                                                      \
                                                                                \
         size_t tail = dequeue->head + dequeue->length;                         \
         if (tail >= dequeue->capacity)                                         \
@@ -86,20 +88,22 @@
                                                                                \
     static inline void dequeue_##name##_add_front(dequeue_##name##_t* dequeue, \
                                                   type el) {                   \
-        if(dequeue->length>=dequeue->capacity){\
-		size_t new_capacity=dequeue->capacity*2;\
-		size_t head_size=dequeue->head+dequeue->length;\
-		head_size-=head_size>dequeue->capacity?dequeue->capacity:0;\
-		dequeue->elems=realloc(dequeue->elems, new_capacity*sizeof(type));\
-		for(size_t i=0; i<head_size;i++){\
-			size_t old_index=dequeue->capacity-i;\
-			size_t new_index=new_capacity-i;\
-			dequeue->elems[new_index]=dequeue->elems[old_index];\
-		}\
-		dequeue->head=new_capacity-head_size;\
-		dequeue->capacity=new_capacity;\
-	}\
-	if (dequeue->head == 0)                                                \
+        if (dequeue->length >= dequeue->capacity) {                            \
+            size_t new_capacity = dequeue->capacity * 2;                       \
+            size_t head_size = dequeue->head + dequeue->length;                \
+            head_size -=                                                       \
+                head_size > dequeue->capacity ? dequeue->capacity : 0;         \
+            dequeue->elems =                                                   \
+                realloc(dequeue->elems, new_capacity * sizeof(type));          \
+            for (size_t i = 0; i < head_size; i++) {                           \
+                size_t old_index = dequeue->capacity - i;                      \
+                size_t new_index = new_capacity - i;                           \
+                dequeue->elems[new_index] = dequeue->elems[old_index];         \
+            }                                                                  \
+            dequeue->head = new_capacity - head_size;                          \
+            dequeue->capacity = new_capacity;                                  \
+        }                                                                      \
+        if (dequeue->head == 0)                                                \
             dequeue->head += dequeue->capacity;                                \
         dequeue->head--;                                                       \
         dequeue->elems[dequeue->head] = el;                                    \
@@ -129,10 +133,10 @@
                                                                                \
     static inline bool dequeue_##name##_is_full(dequeue_##name##_t* dequeue) { \
         return dequeue->length == dequeue->capacity;                           \
-    }\
-	static inline void dequeue_##name##_destroy(dequeue_##name##_t* dequeue){\
-		DEQUEUE_ASSERT(dequeue);\
-		free(dequeue->elems);\
-	}
+    }                                                                          \
+    static inline void dequeue_##name##_destroy(dequeue_##name##_t* dequeue) { \
+        DEQUEUE_ASSERT(dequeue);                                               \
+        free(dequeue->elems);                                                  \
+    }
 
 #endif
